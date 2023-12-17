fcac33f8280d9a34e5932106301bb4fd55ce74c3
Irtaza Ahmed <yrrebeere@gmail.com>
2023-12-18 03:00:10 +0500

    Commit 7.7 (Deployment)

--- ModelDeployment/app.py
+++ ModelDeployment/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
 import pickle
+from joblib import load
 
 app = Flask(__name__)
 
@@ -9,28 +12,77 @@ def home():
 
 @app.route('/predict', methods=["POST"])
 def predict():
-    store_nbr = 1
-    family = ""
-    if request.method == 'POST':
-        family = request.form.get("family")
-    onpromotion = 0
-    typeholiday = "NDay"
-    dcoilwtico = 46.8
-    city = "Quito"
-    state = "Pichincha"
-    typestores = "D"
-    cluster = 13
-    day_of_week = 3
-    day = 16
-    month = 8
-    year = 2017
 
+    data = {
+        'store_nbr': 1,
+        'family_encoded': "",
+        'onpromotion': 0,
+        'typeholiday_encoded': "Holiday",
+        'dcoilwtico': 46.8,
+        'city_encoded': "Quito",
+        'state_encoded': "Pichincha",
+        'typestores_encoded': "D",
+        'cluster': 13,
+        'day_of_week': 3,
+        'day': 16,
+        'month': 8,
+        'year': 2017
+    }
+
+    df = pd.DataFrame([data])
+
+    if request.method == 'POST':
+        df['family_encoded'] = request.form.get('family_encoded')
+    
     with open('models/family_encoder.pkl', 'rb') as file:
         family_encoder = pickle.load(file)
 
-    family = family_encoder.transform([family])[0]
+    with open('models/typeholiday_encoder.pkl', 'rb') as file:
+        typeholiday_encoder = pickle.load(file)
 
-    return render_template('index.html', family = family)
+    with open('models/city_encoder.pkl', 'rb') as file:
+        city_encoder = pickle.load(file)
+
+    with open('models/state_encoder.pkl', 'rb') as file:
+        state_encoder = pickle.load(file)
+
+    with open('models/typestores_encoder.pkl', 'rb') as file:
+        typestores_encoder = pickle.load(file)
+
+    df['family_encoded'] = family_encoder.transform([df['family_encoded'].iloc[0]])[0]
+    df['typeholiday_encoded'] = typeholiday_encoder.transform([df['typeholiday_encoded'].iloc[0]])[0]
+    df['city_encoded'] = city_encoder.transform([df['city_encoded'].iloc[0]])[0]
+    df['state_encoded'] = state_encoder.transform([df['state_encoded'].iloc[0]])[0]
+    df['typestores_encoded'] = typestores_encoder.transform([df['typestores_encoded'].iloc[0]])[0]
+
+    features = [
+        'store_nbr',
+        'onpromotion',
+        'dcoilwtico',
+        'cluster',
+        'day_of_week',
+        'day',
+        'month',
+        'year',
+        'family_encoded',
+        'typeholiday_encoded',
+        'city_encoded', 
+        'state_encoded',
+        'typestores_encoded',
+    ]
+
+    X = df[features]
+
+    model = load('models/M10.joblib')
+
+    prediction = model.predict(X)
+
+    return render_template(
+        'index.html', 
+        prediction = prediction
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+# flask run --port=8000
