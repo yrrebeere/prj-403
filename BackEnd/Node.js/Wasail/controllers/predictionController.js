32e1f74b66158b40c220ad8cb45fdea228e440c0
fizzaadeel <fizza.adeel19@gmail.com>
2024-05-15 17:17:06 +0500

    Commit 36.7 (Monthly Order Recommendation)

--- BackEnd/Node.js/Wasail/controllers/predictionController.js
+++ BackEnd/Node.js/Wasail/controllers/predictionController.js
@@ -19,20 +19,43 @@ const sendWeeklyPrediction = async (req, res) => {
         console.error('Error sending weekly prediction request:', error);
 
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
             res.status(error.response.status).json({ error: error.response.data });
         } else if (error.request) {
-            // The request was made but no response was received
             res.status(500).json({ error: 'No response received from prediction service.' });
         } else {
-            // Something happened in setting up the request that triggered an Error
+            res.status(500).json({ error: 'Error in sending request to prediction service.' });
+        }
+    }
+};
+
+const sendMonthlyPrediction = async (req, res) => {
+    try {
+        const { store_id, product_id } = req.params;
+
+        if (!store_id || !product_id) {
+            return res.status(400).json({ error: 'Store ID and product ID are required.' });
+        }
+
+        const url = `https://hammerhead-app-6m6td.ondigitalocean.app/get-monthly-prediction/${store_id}/${product_id}`;
+
+        const response = await axios.get(url);
+
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Error sending weekly prediction request:', error);
+
+        if (error.response) {
+            res.status(error.response.status).json({ error: error.response.data });
+        } else if (error.request) {
+            res.status(500).json({ error: 'No response received from prediction service.' });
+        } else {
             res.status(500).json({ error: 'Error in sending request to prediction service.' });
         }
     }
 };
 
 module.exports = {
-    sendWeeklyPrediction
+    sendWeeklyPrediction,
+    sendMonthlyPrediction
 
 }
--- BackEnd/Node.js/Wasail/routes/predictions.js
+++ BackEnd/Node.js/Wasail/routes/predictions.js
@@ -4,6 +4,7 @@ const predictionController = require('../controllers/predictionController')
 
 
 router.get('/sendweeklyprediction/:store_id/:product_id',predictionController.sendWeeklyPrediction);
+router.get('/sendmonthlyprediction/:store_id/:product_id',predictionController.sendMonthlyPrediction);
 
 
 module.exports = router;
