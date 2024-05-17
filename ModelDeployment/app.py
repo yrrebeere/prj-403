104bdb09c92620e33181ecf7551c9eecfd5269ca
Irtaza Ahmed <yrrebeere@gmail.com>
2024-05-18 00:40:55 +0500

    Commit 37.7 (Random Forest)

--- ModelDeployment/app.py
+++ ModelDeployment/app.py
@@ -1,32 +1,43 @@
-from flask import Flask, request, jsonify, render_template
-from darts.models import XGBModel
+from flask import Flask, jsonify, render_template
+from darts.models import RandomForest, XGBModel
 import numpy as np
+
 
 app = Flask(__name__)
 
-@app.route('/get-weekly-prediction/<store>/<product>')
-def get_weekly_prediction(store, product):
-    model = XGBModel.load("models/xgb/XGB_S"+store+"P"+product+".pkl")
-    prediction = model.predict(n=7)
+
+@app.route('/get-weekly-prediction/<model>/<store>/<product>')
+def get_weekly_prediction(model, store, product):
+    if model == 'rf':
+        forecasting_model = RandomForest.load("models/rf/RF_S" + store + "P" + product + ".pkl")
+    else:
+        forecasting_model = XGBModel.load("models/xgb/XGB_S" + store + "P" + product + ".pkl")
+    prediction = forecasting_model.predict(n=7)
     prediction = prediction.pd_series().tolist()
     prediction = np.maximum(prediction, 0)
     prediction = sum(prediction)
     prediction = int(prediction)
     return jsonify(prediction), 200
 
-@app.route('/get-monthly-prediction/<store>/<product>')
-def get_monthly_prediction(store, product):
-    model = XGBModel.load("models/xgb/XGB_S" + store + "P" + product + ".pkl")
-    prediction = model.predict(n=30)
+
+@app.route('/get-monthly-prediction/<model>/<store>/<product>')
+def get_monthly_prediction(model, store, product):
+    if model == 'rf':
+        forecasting_model = RandomForest.load("models/rf/RF_S" + store + "P" + product + ".pkl")
+    else:
+        forecasting_model = XGBModel.load("models/xgb/XGB_S" + store + "P" + product + ".pkl")
+    prediction = forecasting_model.predict(n=30)
     prediction = prediction.pd_series().tolist()
     prediction = np.maximum(prediction, 0)
     prediction = sum(prediction)
     prediction = int(prediction)
     return jsonify(prediction), 200
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=["POST"])
 def predict():
@@ -44,5 +55,7 @@ def predict():
         predictions = xgb_predictions,
     )
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
