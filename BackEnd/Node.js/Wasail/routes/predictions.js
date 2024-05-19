ad7bbcb22f257c89568bc1185af3b7038f1f121c
fizzaadeel <fizza.adeel19@gmail.com>
2024-05-19 23:04:35 +0500

    Commit 39.7 (Monthly Prediction)

--- BackEnd/Node.js/Wasail/routes/predictions.js
+++ BackEnd/Node.js/Wasail/routes/predictions.js
@@ -4,7 +4,7 @@ const predictionController = require('../controllers/predictionController')
 
 
 router.get('/sendweeklyprediction/:model/:store_number/:product_number',predictionController.sendWeeklyPrediction);
-router.get('/sendmonthlyprediction/:store_id/:product_id',predictionController.sendMonthlyPrediction);
+router.get('/sendmonthlyprediction/:model/:store_number/:product_number',predictionController.sendMonthlyPrediction);
 
 
 module.exports = router;
