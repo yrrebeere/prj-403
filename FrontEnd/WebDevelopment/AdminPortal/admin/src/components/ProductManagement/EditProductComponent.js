89d73e8cf615720af1fb8b7e2f207eda706d6041
fizzaadeel <fizza.adeel19@gmail.com>
2024-05-21 02:46:07 +0500

    Commit 41.7 (Admin Portal)

--- FrontEnd/WebDevelopment/AdminPortal/admin/src/components/ProductManagement/EditProductComponent.js
+++ FrontEnd/WebDevelopment/AdminPortal/admin/src/components/ProductManagement/EditProductComponent.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import ProductService from '../../services/ProductService';
 import styles from '../../styles/ComponentStyles.css';
 import { useParams, useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 const EditProductComponent = () => {
     const { product_id } = useParams();
     const [productName, setProductName] = useState('');
+    const [imageFile, setImageFile] = useState(null);
     const navigate = useNavigate();
+
+    const handleFileChange = (e) => {
+        setImageFile(e.target.files[0]);
+    };
 
     useEffect(() => {
         async function fetchData() {
@@ -23,16 +29,36 @@ const EditProductComponent = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
         try {
-            console.log(product_id)
-            console.log(productName)
-            await ProductService.editProduct(product_id, {
+            const extension = imageFile ? imageFile.name.split('.').pop() : '';
+
+            const imagePath = `products/${productName}.${extension}`;
+
+            await ProductService.editProduct(product_id,{
                 product_name: productName,
+                image: imagePath,
             });
-            console.log('Product updated successfully');
+
+            console.log('Product added successfully');
+
+            if (imageFile) {
+                const formData = new FormData();
+                formData.append('file', imageFile);
+
+                const apiUrl = `https://sea-lion-app-wbl8m.ondigitalocean.app/api/image/uploadproduct?product_name=${productName}`;
+                await axios.post(apiUrl, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+
+                console.log('Image uploaded successfully');
+            }
+
             navigate('/products');
         } catch (error) {
-            console.error('Error updating product:', error);
+            console.error('Error adding Product:', error);
         }
     };
 
@@ -45,6 +71,12 @@ const EditProductComponent = () => {
                     <tr>
                         <td>Product Name</td>
                         <td><input type="text" value={productName} onChange={(e) => setProductName(e.target.value)}/></td>
+                    </tr>
+                    <tr>
+                        <td>Product Image</td>
+                        <td>
+                            <input type="file" onChange={handleFileChange} />
+                        </td>
                     </tr>
                     </tbody>
                 </table>
